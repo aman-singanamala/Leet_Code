9ea0f7fef53062a84990f7caf2b0805e188c0e5d
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-05-13 18:31:45 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.java
+++ Kadane's Algorithm - GFG/kadanes-algorithm.java
@@ -38,19 +38,36 @@ class Solution{
     //Function to find the sum of contiguous subarray with maximum sum.
     long maxSubarraySum(int arr[], int n){
         
+        // long currentSum = arr[0]; // Initialize currentSum with the first element
+
+        // // Traverse the array starting from the second element
+        // for (int i = 1; i < n; i++) {
+        //     // Calculate the current sum for the subarray ending at index i
+        //     currentSum = Math.max(arr[i], currentSum + arr[i]);
+
+        //     // Update the maximum sum if the current sum is greater
+        //     maxSum = Math.max(maxSum, currentSum);
+        // }
+
+        // return maxSum;long maxSum = arr[0]; // Initialize maxSum with the first element
+        
         long maxSum = arr[0]; // Initialize maxSum with the first element
         long currentSum = arr[0]; // Initialize currentSum with the first element
-
-        // Traverse the array starting from the second element
-        for (int i = 1; i < n; i++) {
-            // Calculate the current sum for the subarray ending at index i
-            currentSum = Math.max(arr[i], currentSum + arr[i]);
-
-            // Update the maximum sum if the current sum is greater
-            maxSum = Math.max(maxSum, currentSum);
+        for(int i=1;i<n;i++){
+            currentSum+= arr[i];
+            if(arr[i]>currentSum){
+            currentSum= arr[i];
         }
-
+        
+            if(currentSum>maxSum){
+                maxSum= currentSum;
+            }
+        }
+        
+        
+        
         return maxSum;
+        
         
         
     }
