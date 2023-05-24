445fa7a0f4bd7238b54789e955b13e65f630c489
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-05-24 10:40:23 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Compute obj = new Compute();
+            long answer[] = obj.printFirstNegativeInteger(a, n, k);
+            int sz = answer.length;
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<sz;i++)
+                output.append(answer[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public long[] printFirstNegativeInteger(long A[], int N, int K)
+    {
+        long[] result= new long[N-K+1];
+        int r=0;
+        for(int i=0;i<=N-K;i++){
+            int count=0;
+            for(int j=i;j<i+K;j++){
+                if(A[j]<0){
+                    result[r++]=A[j];
+                    break;
+                }count++;
+            }
+            
+            if(count==K){
+                result[r++]=0;
+            }
+        }
+        return result;
+    }
+}
