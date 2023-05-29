24256b9c24e2de8170f95fc65ffbde9028c18c39
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-05-29 22:12:44 +0530

    Added solution - LeetHub

--- Activity Selection - GFG/activity-selection.java
+++ Activity Selection - GFG/activity-selection.java
@@ -38,34 +38,20 @@ class Main
 
 // } Driver Code Ends
 
-class Activity implements Comparable<Activity> {
-    int start;
-    int end;
 
-    public Activity(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
 
-    @Override
-    public int compareTo(Activity other) {
-        return this.end - other.end;
-    }
-}
-
-class Solution
-{
-    //Function to find the maximum number of activities that can
-    //be performed by a single person.
-    public static int activitySelection(int start[], int end[], int n)
-    {
-        // add your code here
+class Solution {
+    // Function to find the maximum number of activities that can
+    // be performed by a single person.
+    public static int activitySelection(int start[], int end[], int n) {
+        // Create a list of activities
         List<Activity> activities = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             activities.add(new Activity(start[i], end[i]));
         }
 
-        Collections.sort(activities);
+        // Sort activities based on the end day using a custom comparator
+        activities.sort(Comparator.comparingInt(a -> a.end));
 
         int count = 1;
         int currentEnd = activities.get(0).end;
@@ -78,5 +64,15 @@ class Solution
         }
 
         return count;
+    }
+}
+
+class Activity {
+    int start;
+    int end;
+
+    public Activity(int start, int end) {
+        this.start = start;
+        this.end = end;
     }
 }
