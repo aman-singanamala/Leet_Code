bd344863a6019ced81c91b38404fd3443e24a8c2
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-06-04 16:11:29 +0530

    Added solution - LeetHub

--- Triplet Sum in Array - GFG/triplet-sum-in-array.java
+++ Triplet Sum in Array - GFG/triplet-sum-in-array.java
@@ -39,7 +39,7 @@ class Solution
        // Your code Here
        Arrays.sort(a);
        
-       for(int i=0;i<n;i++){
+       for(int i=0;i<n-2;i++){
           
            int left= i+1;
            int right= n-1;
