8f1fd406154a11a8031b35d5cb58c96e36ac7ce4
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-06-08 18:58:35 +0530

    Added solution - LeetHub

--- Sum of leaf nodes in BST - GFG/sum-of-leaf-nodes-in-bst.java
+++ Sum of leaf nodes in BST - GFG/sum-of-leaf-nodes-in-bst.java
@@ -73,23 +73,17 @@ class GfG
     public static int sumOfLeafNodes(Node root)
     {
         // code here 
-        int ans= helper(root);
-        return ans;
-        
-    }
-    
-    static int helper(Node root){
-        if(root==null){
-            return 0;
+        if (root == null) {
+        return 0;
         }
         
-        if(root.left==null && root.right==null){
+        if (root.left == null && root.right == null) {
             return root.data;
         }
         
-        int leftsum=helper(root.left);
-        int rightsum= helper(root.right);
+        int leftSum = sumOfLeafNodes(root.left);
+        int rightSum = sumOfLeafNodes(root.right);
         
-        return leftsum+rightsum;
+        return leftSum + rightSum;
     }
 }
