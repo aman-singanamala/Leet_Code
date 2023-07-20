d95e069d8febce990dac898e7bbf7a45d926fd98
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-07-20 18:10:04 +0530

    Added solution - LeetHub

--- Detect Loop in linked list - GFG/detect-loop-in-linked-list.java
+++ Detect Loop in linked list - GFG/detect-loop-in-linked-list.java
@@ -81,19 +81,6 @@ class Solution {
     //Function to check if the linked list has a loop.
     public static boolean detectLoop(Node head){
         // Add code here
-        // Node fast= head;
-        // Node slow= head;
-        // while(fast!=null && fast.next!=null){
-        //     fast= fast.next.next;
-        //     slow=slow.next;
-        //     if(slow==fast){
-        //         return true;
-        //     }
-        // }
-        
-        // return false;
-        
-        
         Node slow= head;
         Node fast= head;
         while(fast!=null && fast.next!=null){
@@ -103,7 +90,6 @@ class Solution {
                 return true;
             }
         }
-        
         return false;
     }
 }
