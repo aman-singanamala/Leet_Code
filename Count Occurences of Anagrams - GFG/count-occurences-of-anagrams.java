340dbd8991a146fac9f7c3d423d05c6cda6b54c9
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-07-28 08:36:06 +0530

    Added solution - LeetHub

--- Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.java
+++ Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.java
@@ -25,49 +25,50 @@ class GFG {
 
 // User function Template for Java
 
+
 class Solution {
-    boolean allZeros(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=0){
-                return false;
-            }
-        }
-        return true;
-    }
 
     int search(String pat, String txt) {
-        // code here
-        int n= txt.length();
-        int k= pat.length();
-        
-        if(n==0 ||k==0 ){
-            return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> m = new HashMap<>();
+
+        // Initialize the frequency count for characters in the pattern.
+        for (int i = 0; i < pat.length(); i++) {
+            char c = pat.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int result=0;
-        
-        int[] map= new int[26];
-        for(char c: pat.toCharArray()){
-            map[c-'a']++;
-        }
-        
-        int i=0,j=0;
-        while(j<n){
-            map[txt.charAt(j)-'a']--;
-            
-            if(j-i+1==k){
-                if(allZeros(map)){
-                    result++;
+
+        int count = 0;
+        int i = 0;
+        int j = 0;
+
+        while (j < txt.length()) {
+            char right = txt.charAt(j);
+
+            // Include the right character in the window.
+            m.put(right, m.getOrDefault(right, 0) + 1);
+
+            // Expand the window until it reaches the size of the pattern.
+            if (j - i + 1 < pat.length()) {
+                j++;
+            } else if (j - i + 1 == pat.length()) {
+                // Check if the current window matches the pattern.
+                if (m.equals(map)) {
+                    count++;
                 }
-                map[txt.charAt(i)-'a']++;
+
+                // Slide the window and remove the left character from the window.
+                char left = txt.charAt(i);
+                m.put(left, m.get(left) - 1);
+                if (m.get(left) == 0) {
+                    m.remove(left);
+                }
+
                 i++;
+                j++;
             }
-            j++;
-            
-            
-            
         }
-        
-        return result;
-        
+
+        return count;
     }
 }
