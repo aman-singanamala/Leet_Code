9a11d805f475f8217ffd55524c287186f75a30e0
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-08-21 10:32:54 +0530

    Added solution - LeetHub

--- Count the triplets - GFG/count-the-triplets.java
+++ Count the triplets - GFG/count-the-triplets.java
@@ -32,23 +32,25 @@ class GFG {
 class Solution {
     int countTriplet(int arr[], int n) {
         // code here
-        Arrays.sort(arr);
         int count=0;
-        for(int i=n-1;i>=0;i--){
-            int right=n-1;
-            int left=0;
+        Arrays.sort(arr);
+        for(int i=n-1;i>=2;i--){
+            int left= 0;
+            int right= i-1;
             while(left<right){
-                if(arr[left]+arr[right]==arr[i]){
+                if(arr[left]+ arr[right]==arr[i]){
                     count++;
                     left++;
                     right--;
-                }else if(arr[left]+arr[right]<arr[i]){
+                }else if(arr[left]+ arr[right]<arr[i]){
                     left++;
                 }else{
                     right--;
                 }
             }
+            
         }
         return count;
+        
     }
 }
