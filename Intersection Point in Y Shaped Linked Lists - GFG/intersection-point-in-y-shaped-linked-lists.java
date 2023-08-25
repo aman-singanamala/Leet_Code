9d61025c8241caf42ccf1e3689fd34c86839e240
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-08-26 00:36:56 +0530

    Added solution - LeetHub

--- Intersection Point in Y Shaped Linked Lists - GFG/intersection-point-in-y-shaped-linked-lists.java
+++ Intersection Point in Y Shaped Linked Lists - GFG/intersection-point-in-y-shaped-linked-lists.java
@@ -1,6 +1,6 @@
 //{ Driver Code Starts
 import java.util.*;
-
+import java.io.*;
 
 class Node
 {
@@ -35,76 +35,81 @@ public void addToTheLast(Node node)
 }
 
   /* Function to print linked list */
-    void printList()
+    void printList(PrintWriter out)
     {
         Node temp = head;
         while (temp != null)
         {
-           System.out.print(temp.data+" ");
+           out.print(temp.data+" ");
            temp = temp.next;
         }  
-        System.out.println();
+        out.println();
     }
 	
 	 
  
      /* Driver program to test above functions */
-    public static void main(String args[])
+    public static void main(String args[])throws IOException
     {
        
          
         /* Constructed Linked List is 1->2->3->4->5->6->
            7->8->8->9->null */
-         Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
+         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+         PrintWriter out=new PrintWriter(System.out);
+		 int t=Integer.parseInt(in.readLine().trim());
 		 
 		 while(t>0)
          {
-			int n1 = sc.nextInt();
-			int n2 = sc.nextInt();
-			int n3 = sc.nextInt();
+            String s[]=in.readLine().trim().split(" ");
+			int n1 = Integer.parseInt(s[0]);
+			int n2 = Integer.parseInt(s[1]);
+			int n3 = Integer.parseInt(s[2]);
 			LinkedList_Intersection llist1 = new LinkedList_Intersection();
 		    LinkedList_Intersection llist2 = new LinkedList_Intersection();
 			LinkedList_Intersection llist3 = new LinkedList_Intersection();
 			
-				int a1=sc.nextInt();
+			    s=in.readLine().trim().split(" ");
+				int a1=Integer.parseInt(s[0]);
 				Node head1= new Node(a1);
 				Node tail1= head1;
 				
 				for (int i = 1; i < n1; i++) 
 				{
-					int a = sc.nextInt(); 
+					int a = Integer.parseInt(s[i]); 
 					tail1.next = (new Node(a));
 					tail1= tail1.next;
 				}
 			
-			
-				int b1=sc.nextInt();
+			    s=in.readLine().trim().split(" ");
+				int b1=Integer.parseInt(s[0]);
 				Node head2 = new Node(b1);
 				Node tail2 = head2;
 				for (int i = 1; i < n2; i++) 
 				{
-					int b = sc.nextInt();  
+					int b = Integer.parseInt(s[i]);  
 					tail2.next = (new Node(b));
 					tail2= tail2.next;
 				}
-				if(n3>0){
-				int c1=sc.nextInt();
+				
+				s=in.readLine().trim().split(" ");
+				int c1=Integer.parseInt(s[0]);
 				Node head3= new Node(c1);
 				tail1.next = head3;
 				tail2.next = head3;
 				Node tail3=head3;
 				for (int i = 1; i < n3; i++) 
 				{
-					int c = sc.nextInt();   
+					int c = Integer.parseInt(s[i]); 
 					tail3.next = (new Node(c));
 					tail3= tail3.next;
 				}
-				}
+				
 				Intersect obj = new Intersect();
-				System.out.println(obj.intersectPoint(head1, head2));
+				out.println(obj.intersectPoint(head1, head2));
 			t--;			
          }
+         out.close();
     }
 }
 
@@ -128,45 +133,45 @@ class Intersect
     //Function to find intersection point in Y shaped Linked Lists.
 	int intersectPoint(Node head1, Node head2)
 	{
-        // code here
-        Node curr1=head1;
-        Node curr2=head2;
-        int ans;
+         // code here
         int size1= getsize(head1);
         int size2= getsize(head2);
-        int diff= Math.abs(size1-size2);
-         // Move the pointer of the longer linked list ahead by the difference
-        if (size1 > size2) {
-            while (diff > 0) {
-                curr1 = curr1.next;
-                diff--;
+        
+        int k= Math.abs(size1- size2);
+        
+        if(size1> size2){
+            while(k>0){
+                head1= head1.next;
+                k--;
             }
-        } else {
-            while (diff > 0) {
-                curr2 = curr2.next;
-                diff--;
+        }else{
+            while(k>0){
+                head2= head2.next;
+                k--;
             }
         }
-        while(curr1!=null && curr2!=null){
-            if(curr1==curr2){
-                return curr1.data;
+        
+        while(head1!=null && head2!=null){
+            if(head1==head2){
+                return head1.data;
             }
-            curr1= curr1.next;
-            curr2= curr2.next;
+            
+            head1= head1.next;
+            head2= head2.next;
         }
+        
         return -1;
-         
 	}
-	int getsize(Node head){
+	static int getsize(Node head){
 	    int size=0;
 	    Node curr= head;
 	    while(curr!=null){
+	        curr= curr.next;
 	        size++;
-	        curr=curr.next;
 	    }
+	    
 	    return size;
 	}
-	
 }
 
 
