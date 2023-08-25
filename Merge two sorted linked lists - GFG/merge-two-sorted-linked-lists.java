345d1123e168ba5e3474fb0902712f73bc62265a
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2023-08-25 23:36:09 +0530

    Added solution - LeetHub

--- Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.java
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.java
@@ -92,19 +92,11 @@ class LinkedList
     Node sortedMerge(Node head1, Node head2) {
      // This is a "method-only" submission. 
      // You only need to complete this method
-        Node dummy= new Node(0);
-        Node curr= dummy;
-        
-        if(head1==null){
-            return head2;
-        }
-        if(head2==null){
-            return head1;
-        }
-        
+     Node temp= new Node(0);
+        Node curr= temp;
         while(head1!=null && head2!=null){
-            if(head1.data <= head2.data){
-                curr.next= head1;
+            if(head1.data<= head2.data){
+                curr.next=head1;
                 head1= head1.next;
             }else{
                 curr.next= head2;
@@ -112,19 +104,18 @@ class LinkedList
             }
             curr= curr.next;
         }
-        
+
         while(head1!=null){
             curr.next= head1;
             curr= curr.next;
             head1= head1.next;
         }
-        
         while(head2!=null){
             curr.next= head2;
-            curr=curr.next;
+            curr= curr.next;
             head2= head2.next;
         }
-        
-        return dummy.next;
+
+        return temp.next;
    } 
 }
